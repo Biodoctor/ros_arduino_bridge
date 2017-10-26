2e2089e43adcb054dc748deec15ec6f765abd8ea
Biodoctor <renato.gaspar@gmail.com>
2017-10-26 12:29:40 -0200

    Added config option to VNH2SP30 dual motor driver shield

--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.ino
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.ino
@@ -86,6 +86,29 @@
     setMotorSpeed(LEFT, leftSpeed);
     setMotorSpeed(RIGHT, rightSpeed);
   }
+#elif defined VNH2SP30_MOTOR_DRIVER
+  /* Include the Pololu based VNH2SP30 library */
+  #include "DualVNH5019MotorShield.h"
+
+  /* Create the motor driver object */
+  DualVNH5019MotorShield drive;
+  
+  /* Wrap the motor driver initialization */
+  void initMotorController() {
+    drive.init();
+  }
+
+  /* Wrap the drive motor set speed function */
+  void setMotorSpeed(int i, int spd) {
+    if (i == LEFT) drive.setM1Speed(spd);
+    else drive.setM2Speed(spd);
+  }
+
+  // A convenience function for setting both motor speeds
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    setMotorSpeed(LEFT, leftSpeed);
+    setMotorSpeed(RIGHT, rightSpeed);
+  }
 #else
   #error A motor driver must be selected!
 #endif
